f58c4c35f3aa13d09232476a1413aa16ed1da5dc
Oleg Telegin <teleginos@mail.ru>
2023-05-01 14:41:33 +0300

    add solution task19

---
+++ Task19/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Введите число для определения является ли оно палиндромом (число должно быть не менее 2-х знаков): ");
+string number = Console.ReadLine();
+string Flag = "True";
+
+// Console.WriteLine(number[number.Length - 1]);
+
+if (number.All(char.IsDigit) && number.Length >= 2)
+{
+    for(int i = 0; i <= number.Length / 2; i += 1)
+    {
+        if(number[i] != number[number.Length - 1 - i])
+        {
+            Flag = "False";
+        }
+    }
+    if(Flag == "True")
+    {
+        Console.WriteLine($"Число {number} является палиндромом");
+    }
+    else
+    {
+    Console.WriteLine($"Число {number} не является палиндромом"); 
+    }
+}
+else
+{
+    Console.WriteLine("Введены некорректные данные");
+}
+
