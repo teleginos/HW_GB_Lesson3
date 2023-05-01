19b8a1691e9129e6bf905317ac2d8c6b52b5b171
Oleg Telegin <teleginos@mail.ru>
2023-05-01 17:27:22 +0300

    add solution task23

---
+++ Task23/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+for(int i = 1; i <= number; i += 1)
+{
+    if(i != number)
+    {
+        Console.Write($"{Math.Pow(i, 3)}, ");
+    }
+    else
+    {
+        Console.Write(Math.Pow(i, 3));
+    }
+}
